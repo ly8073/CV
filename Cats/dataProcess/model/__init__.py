bb87d779d588286abc289fa9c2c96dfd869fce87
ly8073 <807394891@qq.com>
2022-07-23 19:51:43 +0800

    删除了ResNet，打算自己实现

---
+++ Cats/dataProcess/model/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2022/5/16 22:15
+# @Author  : Ly
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @Github  : https://github.com/ly8073
